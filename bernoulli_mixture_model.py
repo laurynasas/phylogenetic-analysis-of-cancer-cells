440c39f65e7d01309e8ac6c741938f03e67932a7
Laurynas Tamulevicus <laurynas@opinew.com>
2016-11-16 01:34:47 +0000

    finished bernoulli

--- bernoulli_mixture_model.py
+++ bernoulli_mixture_model.py
@@ -63,19 +63,67 @@ def log_likelihood_complete_data(likelihood):
     return log(likelihood)
 
 
+def expectation_step(z_matrix, pi_proportion_of_images, miu_pixel_distributions):
+    n_images, k_clusters = z_matrix.shape
+
+    for n in xrange(n_images):
+        x_vector = x_vectors[n]
+        for k in xrange(k_clusters):
+            nom = pi_proportion_of_images[k] * prob_x_generated_by_cluster_k(x_vector, miu_pixel_distributions, k)
+            den = 0
+            for m in xrange(k_clusters):
+                den += pi_proportion_of_images[m] * prob_x_generated_by_cluster_k(x_vector, miu_pixel_distributions, m)
+
+            z_matrix[n, k] = nom * 1.0 / den
+
+    return z_matrix
+
+
+def N_m_effective_number_of_images(z_matrix, k_cluster):
+    total = 0
+    for n in xrange(n_images):
+        total += z_matrix[n, k_cluster]
+    return total
+
+
+def maximize_miu(miu_pixel_distributions, z_matrix, x_vectors):
+
+    for m in xrange(k_clusters):
+        for j in xrange(d_pixels):
+            N_m = N_m_effective_number_of_images(n_images, z_matrix, m)
+
+            total = sum(x_vectors[n, j] * z_matrix[n, m] for n in xrange(n_images))
+
+            miu_pixel_distributions[m, j] = total * 1.0 / N_m
+
+    return miu_pixel_distributions
+
+
+def maximize_pi(n_images, z_matrix, x_vectors, k_clusters, pi_proportion_of_images):
+    for m in xrange(k_clusters):
+        N_m = N_m_effective_number_of_images(n_images, z_matrix, m)
+        pi_proportion_of_images[m] = N_m * 1.0 / n_images
+
+    return pi_proportion_of_images
+
+
 if __name__ == '__main__':
     set_printoptions(threshold=nan)
-    dir = "./data/sample_data.csv"
-    unique_rows = process_file(dir)
+    directory = "./data/sample_data.csv"
+    unique_rows = process_file(directory)
+    global n_images, d_pixels, k_clusters, x_vectors
 
+    x_vectors = []
+    for key in unique_rows.keys():
+        rep = unique_rows[key]
+        x_vector = [int(el) for el in key.split(",")]
+        for i in xrange(rep):
+            x_vectors.append(x_vector)
+
+    x_vectors = array(x_vectors)
     n_images = sum(unique_rows[key] for key in unique_rows.keys())
-
     d_pixels = len(unique_rows.keys()[0].split(","))
-
     k_clusters = 12
-
-    print n_images, d_pixels
-    print unique_rows
 
     miu_pixel_distributions = zeros((d_pixels, k_clusters))
 
@@ -91,7 +139,6 @@ if __name__ == '__main__':
     print sum(z_matrix, axis=1)
     pi_proportion_of_images = sum(z_matrix, axis=1) * 1.0 / n_images
 
-    print pi_proportion_of_images
     # print z_matrix
     # pi_proportion_of_images = pi_proportion_of_images*1.0 / k_clusters
     # occ_dict = count_occurences(pi_proportion_of_images)
--- processing_sample_data.py
+++ processing_sample_data.py
@@ -1,51 +1,50 @@
-import numpy as np
 from single_linkage_clustering import *
 
+
 def process_file(dir):
-	sample_data_file = open(dir, 'rw+')
-	unique_rows = {}
-	sample_data_file.readline()
+    sample_data_file = open(dir, 'rw+')
+    unique_rows = {}
+    sample_data_file.readline()
 
-	for line in sample_data_file:
-		line = line[:-1]
-		line = ','.join([str(x) for x in (line.split(",")[:-1])])
+    for line in sample_data_file:
+        line = line[:-1]
+        line = ','.join([str(x) for x in (line.split(",")[:-1])])
 
-		if line in unique_rows.keys():
-			unique_rows[line] +=1
-		else:
-			unique_rows[line] = 1
+        if line in unique_rows.keys():
+            unique_rows[line] += 1
+        else:
+            unique_rows[line] = 1
 
-	return unique_rows
+    return unique_rows
 
 
-def diff_char_string(a,b):
-	distance = 0
-	for i,j in zip(a,b):
-		if i != j:
-			distance+=1
-	return distance
+def diff_char_string(a, b):
+    distance = 0
+    for i, j in zip(a, b):
+        if i != j:
+            distance += 1
+    return distance
 
 
 def find_distance_matrix(unique_rows):
-	number_rows = len(unique_rows.keys())
-	unique_strings = unique_rows.keys()
+    number_rows = len(unique_rows.keys())
+    unique_strings = unique_rows.keys()
 
-	distance_matrix = []
-	for i in range(number_rows):
-		new_row = []
-		for j in range(number_rows):
-			new_row +=[diff_char_string(unique_strings[i], unique_strings[j])]
-		distance_matrix+=[new_row]
+    distance_matrix = []
+    for i in range(number_rows):
+        new_row = []
+        for j in range(number_rows):
+            new_row += [diff_char_string(unique_strings[i], unique_strings[j])]
+        distance_matrix += [new_row]
 
-	return distance_matrix
-
+    return distance_matrix
 
 
 if __name__ == '__main__':
-	dir = "./data/sample_data.csv"
-	unique_rows = process_file(dir)
-	# print unique_rows
-	distance_matrix = np.matrix(find_distance_matrix(unique_rows))
-	# distance_matrix = np.matrix([[0,662,877,255,412,996],[662,0,295,468,268,400],[877,295,0,754,564,138],[255,468,754,0,219,869],[412,268,564,219,0,669],[996,400,138,869,669,0]])
-	# print "--->\n",distance_matrix
-	print single_linkage_clustering(distance_matrix)
+    dir = "./data/sample_data.csv"
+    unique_rows = process_file(dir)
+    # print unique_rows
+    distance_matrix = np.matrix(find_distance_matrix(unique_rows))
+    # distance_matrix = np.matrix([[0,662,877,255,412,996],[662,0,295,468,268,400],[877,295,0,754,564,138],[255,468,754,0,219,869],[412,268,564,219,0,669],[996,400,138,869,669,0]])
+    # print "--->\n",distance_matrix
+    print single_linkage_clustering(distance_matrix)
